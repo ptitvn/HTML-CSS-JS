00d5d569a8b5103b8e4856b0ea61df10215f31a3
PTITVN <tle474498@gmail.com>
2025-03-11 21:33:07 +0700

    session25

--- session25/baitap/bt9.js
+++ session25/baitap/bt9.js
@@ -1,22 +1,23 @@
 let users= [];
-function isValidEmail(string) {
-    let testString = string.includes("@");
-    let test = string.endsWith(".com")
-    let test2 = string.endsWith(".vn")
-    
-}
 function string(email){
-    if (isValidEmail(email)) {
-        console.log("Email không hợp lệ. Vui lòng nhập lại.");
-        return;
+    for(let i =0;i<email.length;i++){
+        if(!email.includes("@") && !email.endsWith(".com"||!email.endsWith(".vn"))        ){
+           
+                console.log("Email không hợp lệ. Vui lòng nhập lại.");
+                return;
+            
+        }
     }
-
     if (users.includes(email)) {
         console.log("Tài khoản đã tồn tại.");
     } else {
         users.push(email);
         console.log("Đăng ký thành công.");
+        console.log(users);
     }
+    
+
+    
     
 }
 let email = prompt("Mời bạn nhập email");
