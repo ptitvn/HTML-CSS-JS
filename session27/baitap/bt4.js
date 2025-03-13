049d9d929bd911fe061f04154f2aa79b6f5b8576
PTITVN <tle474498@gmail.com>
2025-03-13 17:11:07 +0700

    session27

--- session27/baitap/bt4.js
+++ session27/baitap/bt4.js
@@ -13,10 +13,14 @@ function main(){
         switch (menu) {
             case 1:
                 let n = +prompt("Mời bạn nhập vào số lượng số nguyên muốn nhập vào");
-                for(let i = 0;i<n;i++){
-                    let numbers = +prompt("Mời bạn nhập vào số nguyên");
-                    arr.push(numbers);
+                if(Number.isInteger(n)){
+                    for(let i = 0;i<n;i++){
+                        let numbers = +prompt("Mời bạn nhập vào số nguyên");
+                        if(Number.isInteger(numbers)){arr.push(numbers);}
+                        
+                    }
                 }
+                
                 
                 break;
             case 2:
@@ -26,11 +30,22 @@ function main(){
                 break;
             case 3:
                 let maxEven = Math.max(...arr.filter(num => num % 2 === 0));
-                console.log(`Số chẵn lớn nhất: ${maxEven}`);
+                if(maxEven === !-Infinity){
+                    console.log(`Số chẵn lớn nhất: ${maxEven}`);
+                }else{
+                    console.log("Không có phần tử chẵn");
+                }
+                
                 break;
             case 4:
                 let minOdd = Math.min(...arr.filter(num => num % 2 !== 0));
-                console.log(`Số lẻ nhỏ nhất: ${minOdd}`);       
+                if(minOdd === !-Infinity){
+                    console.log(`Số lẻ nhỏ nhất: ${minOdd}`); 
+                }else{
+                    console.log("Không có phần tử lẻ");
+                    
+                }
+                      
                 break;
                 
         default:
